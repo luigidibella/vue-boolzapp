cae31f948765b89062580946c0d5f5733a898be7
luigidibella <lykos.omega@gmail.com>
2024-03-14 23:49:54 +0100

    [add] updateFormattedDateTime methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ createApp({
       counter: 0,
       newText: '',
       searchUser: '',
+      formattedDateTime: '',
       
     }
   },
@@ -16,18 +17,30 @@ createApp({
   methods: {
     addText(index){
       this.contacts[index].messages.push({
-      date: '10/01/2020 15:50:00',
+      date: this.formattedDateTime,
       message: this.newText,
       status: 'sent',
       });
       this.newText = '';
       setTimeout(() => {
         this.contacts[index].messages.push({
-          date: '10/01/2020 16:15:22',
+          date: this.formattedDateTime,
           message: 'Ok!',
           status: 'received'
         });
       }, 1000);
+    },
+
+    updateFormattedDateTime() {
+      const currentDate = new Date();
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+      const year = currentDate.getFullYear();
+      const hours = String(currentDate.getHours()).padStart(2, '0');
+      const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+      const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+      this.formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
     },
     
   },
@@ -47,6 +60,7 @@ createApp({
   },
 
   mounted() {
+    this.updateFormattedDateTime();
     console.log();
   },
 }).mount('#app')
